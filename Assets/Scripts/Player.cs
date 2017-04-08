0ef1e6420e4e368bb7d837e48b6bf8bcac9eea4a
Conrad Pinnow <conrad.pinnow@gmail.com>
2017-04-07 21:52:55 -0700

    added animation and fixed player drifting issue

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,11 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private Ship ship;
-    
+
     // Use this for initialization
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
@@ -23,11 +23,8 @@ public class Player : MonoBehaviour
 
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        if (x != 0 || y != 0)
-        {
-            ship.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        }
-        
+        ship.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
         if (Input.GetButton("Fire1"))
         {
             ship.Shoot();
